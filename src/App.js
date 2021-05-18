70d071d34ffe36599c2f5f5698b7b118d29920b9
koren wohl <korenwohl@gmail.com>
2021-05-18 19:10:37 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
-import { FaRegHandshake } from 'react-icons/fa';
+import { FaPaperPlane } from 'react-icons/fa';
 
 function App () {
   return (
     <div className="todo-app">
         <a href="https://korenw.netlify.app/">
-           <FaRegHandshake />
+           <FaPaperPlane />
         </a>
         <h1>Todo app</h1>
         <TodoList />
