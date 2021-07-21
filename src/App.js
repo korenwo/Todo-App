b0da708a1a508d1dc5b67703aa5d4918134302bd
koren wohl <korenwohl@gmail.com>
2021-07-21 15:06:40 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
-import { FaPaperPlane } from 'react-icons/fa';
 
 function App () {
   return (
     <div className="todo-app">
-        <a href="https://korenw.netlify.app/">
-           <FaPaperPlane />
-        </a>
         <h1>Todo app</h1>
         <TodoList />
     </div>
